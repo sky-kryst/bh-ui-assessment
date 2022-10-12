4be1b00650ea8f6f9b9ea71ff8b9e92f2ec93ca2
sky-kryst <aakashmeshram@outlook.com>
2022-10-13 03:10:22 +0530

    refactor(App.tsx): :recycle: use auth store

--- App.tsx
+++ App.tsx
@@ -2,13 +2,17 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
 import { AuthStack } from "./navigation";
+import { Home } from "./screens";
+import { useAuthStore } from "./stores";
 
 export default function App() {
+  const isUserAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
       <NavigationContainer>
-        <AuthStack />
+        {isUserAuthenticated ? <Home /> : <AuthStack />}
       </NavigationContainer>
     </SafeAreaView>
   );
--- stores/auth.ts
+++ stores/auth.ts
@@ -1,6 +1,12 @@
 import create from "zustand";
 
-export const useAuthStore = create((set) => ({
+interface IAuthStore {
+  isAuthenticated: boolean
+  authorizeUser: () => void
+  unAuthorizeUser: () => void
+}
+
+export const useAuthStore = create<IAuthStore>((set) => ({
   isAuthenticated: false,
   authorizeUser: () => set(() => ({ isAuthenticated: true })),
   unAuthorizeUser: () => set(() => ({ isAuthenticated: false })),
