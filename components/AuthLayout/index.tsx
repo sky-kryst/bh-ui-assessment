0e052bfe2d5e83d02eb405d5c82e83a787372f08
sky-kryst <aakashmeshram@outlook.com>
2022-10-13 21:27:42 +0530

    feat(components): :sparkles: add banner animation

--- components/AuthLayout/index.tsx
+++ components/AuthLayout/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Image,
   Keyboard,
@@ -8,6 +8,12 @@ import {
   TouchableWithoutFeedback,
   View,
 } from "react-native";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 import { screenHeight } from "../../constants";
 import styles from "./styles";
 
@@ -24,6 +30,72 @@ export const AuthLayout = ({
   keyboardVerticalOffset,
   headerHeight,
 }: IProps) => {
+  const firstImageOpacity = useSharedValue(1);
+  const secondImageOpacity = useSharedValue(0);
+  const firstImageYOffset = useSharedValue(0);
+  const secondImageYOffset = useSharedValue(0);
+
+  useEffect(() => {
+    const keyboardWillShow = Keyboard.addListener("keyboardWillShow", () => {
+      firstImageOpacity.value = withTiming(0, {
+        duration: 2500,
+        easing: Easing.out(Easing.linear),
+      });
+      firstImageYOffset.value = withTiming(-20, {
+        duration: 2500,
+        easing: Easing.out(Easing.exp),
+      });
+      secondImageOpacity.value = withTiming(1, {
+        duration: 1200,
+        easing: Easing.out(Easing.exp),
+      });
+    });
+    const keyboardWillHide = Keyboard.addListener("keyboardWillHide", () => {
+      firstImageOpacity.value = withTiming(1, {
+        duration: 1500,
+        easing: Easing.out(Easing.exp),
+      });
+      firstImageYOffset.value = withTiming(0, {
+        duration: 1500,
+        easing: Easing.out(Easing.exp),
+      });
+
+      secondImageOpacity.value = withTiming(0, {
+        duration: 1000,
+        easing: Easing.out(Easing.exp),
+      });
+      secondImageYOffset.value = withTiming(
+        -20,
+        {
+          duration: 500,
+          easing: Easing.out(Easing.linear),
+        },
+        () => {
+          secondImageYOffset.value = 0;
+        }
+      );
+    });
+
+    return () => {
+      keyboardWillShow.remove();
+      keyboardWillHide.remove();
+    };
+  }, []);
+
+  const firstImageStyles = useAnimatedStyle(() => {
+    return {
+      opacity: firstImageOpacity.value,
+      transform: [{ translateY: firstImageYOffset.value }],
+    };
+  });
+
+  const secondImageStyles = useAnimatedStyle(() => {
+    return {
+      opacity: secondImageOpacity.value,
+      transform: [{ translateY: secondImageYOffset.value }],
+    };
+  });
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -32,13 +104,29 @@ export const AuthLayout = ({
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.Container}>
-          <View style={styles.BannerContainer}>
+          <Animated.View style={[styles.BannerContainer, firstImageStyles]}>
             <Image
               source={require("../../assets/images/login.png")}
               style={styles.BannerImage}
               resizeMode="stretch"
             />
-          </View>
+          </Animated.View>
+          <Animated.View
+            style={[
+              styles.BannerContainer,
+              {
+                position: "absolute",
+                width: "100%",
+              },
+              secondImageStyles,
+            ]}
+          >
+            <Image
+              source={require("../../assets/images/login2.png")}
+              style={styles.BannerImage}
+              resizeMode="stretch"
+            />
+          </Animated.View>
           <View
             style={[
               styles.HeaderContainer,
