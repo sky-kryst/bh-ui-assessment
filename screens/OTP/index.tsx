d5c355a4189f9d397a21c316afc8a44cf9905c8a
sky-kryst <aakashmeshram@outlook.com>
2022-10-13 14:24:23 +0530

    feat(screens): :sparkles:

--- screens/OTP/index.tsx
+++ screens/OTP/index.tsx
@@ -1,7 +1,7 @@
 import React, { Component, createRef } from "react";
 import { Text, TextInput, View } from "react-native";
-import { AuthLayout, PrimaryButton } from "../../components";
-import { useLoginDetailsStore } from "../../stores";
+import { AuthLayout, BlueLink, PrimaryButton } from "../../components";
+import { useAuthStore, useLoginDetailsStore } from "../../stores";
 import styles from "./styles";
 
 const noOfInputsArr = [1, 2, 3, 4, 5, 6];
@@ -17,6 +17,9 @@ export class OTP extends Component {
       }),
       {}
     ),
+    isError: false,
+    isLoading: false,
+    errorString: "",
   };
 
   inputRef = noOfInputsArr.reduce(
@@ -55,11 +58,11 @@ export class OTP extends Component {
       }
 
       if (keyValue === "Backspace") {
-        // if (this.state.otp[`${key}`]) {
-        //   this.setState({
-        //     otp: { ...this.state.otp, [`${key}`]: "" },
-        //   });
-        // } else {
+        if (this.state.otp[`${key}`]) {
+          this.setState({
+            otp: { ...this.state.otp, [`${key}`]: "" },
+          });
+        }
         this.inputRef[`${key - 1}`]?.current.focus();
         // }
         return;
@@ -72,41 +75,89 @@ export class OTP extends Component {
     };
   };
 
+  onFormSubmit = () => {
+    const otpLength = (
+      Object.values(this.state.otp).reduce(
+        (accumulator: string, currentElement) =>
+          (accumulator += currentElement),
+        ""
+      ) as string
+    ).length;
+
+    if (otpLength < 6) {
+      this.setState({ errorString: "OTP at least 6 digits", isError: true });
+      return;
+    }
+
+    if (otpLength < 1) {
+      this.setState({
+        errorString: "Please enter the OTP sent to you",
+        isError: true,
+      });
+      return;
+    }
+
+    this.setState({ errorString: "", isError: false, isLoading: true });
+
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+      useAuthStore.setState({ isAuthenticated: true });
+    }, 1500);
+  };
+
   render() {
     return (
       <AuthLayout
         headerText={
-          <View>
+          <View style={styles.Header}>
             <Text style={{ fontWeight: "bold" }}>
               Enter 6 digit OTP sent on
             </Text>
-            <Text>
-              {useLoginDetailsStore.getState().countryCode}{" "}
-              {useLoginDetailsStore.getState().phoneNumber}
-            </Text>
+            <View style={styles.PhoneNumber}>
+              <Text>
+                {useLoginDetailsStore.getState().countryCode}
+                {"-"}
+                {useLoginDetailsStore.getState().phoneNumber}
+              </Text>
+              <BlueLink
+                style={{ fontWeight: "600", textDecorationLine: "underline" }}
+              >
+                Change
+              </BlueLink>
+            </View>
           </View>
         }
       >
         <View style={styles.Form}>
-          <View style={[styles.InputContainer, { width: "100%" }]}>
-            {noOfInputsArr.map((currentElement, currentIndex) => (
-              <TextInput
-                key={String(currentElement)}
-                maxLength={/* noOfInputs - currentIndex */ 1}
-                keyboardType="number-pad"
-                textContentType="oneTimeCode"
-                textAlign="center"
-                style={[styles.Input, { borderWidth: 1 }]}
-                value={this.state.otp[`${currentElement}`]}
-                ref={this.inputRef[`${currentElement}`]}
-                onChangeText={this.onInputChange(currentElement)}
-                onKeyPress={this.onKeyPressed(currentElement)}
-              />
-            ))}
+          <View style={styles.InputContainer}>
+            <View style={[styles.Inputs]}>
+              {noOfInputsArr.map((currentElement, currentIndex) => (
+                <TextInput
+                  key={String(currentElement)}
+                  maxLength={/* noOfInputs - currentIndex */ 1}
+                  keyboardType="number-pad"
+                  textContentType="oneTimeCode"
+                  textAlign="center"
+                  style={[
+                    styles.Input,
+                    this.state.isError && { borderColor: "red" },
+                  ]}
+                  value={this.state.otp[`${currentElement}`]}
+                  ref={this.inputRef[`${currentElement}`]}
+                  // onChangeText={this.onInputChange(currentElement)}
+                  onKeyPress={this.onKeyPressed(currentElement)}
+                />
+              ))}
+            </View>
+            <BlueLink>{this.state.isError && "Bingo"}</BlueLink>
           </View>
-          <View>
-            <PrimaryButton buttonText="Submit" />
-            <Text>Resend OTP 30s</Text>
+          <View style={styles.Buttons}>
+            <PrimaryButton buttonText="Submit" onPress={this.onFormSubmit} />
+            {this.state.isError ? (
+              <BlueLink>Resend OTP</BlueLink>
+            ) : (
+              <Text>Resend OTP 30s</Text>
+            )}
           </View>
         </View>
       </AuthLayout>
--- screens/OTP/styles.ts
+++ screens/OTP/styles.ts
@@ -1,4 +1,8 @@
 import { StyleSheet } from "react-native";
+import { screenHeight, screenWidth } from "../../constants";
+
+const isWidthSmall = screenWidth < 400;
+const isHeightSmall = screenHeight < 650;
 
 export default StyleSheet.create({
   Form: {
@@ -7,12 +11,38 @@ export default StyleSheet.create({
     flex: 1,
   },
   InputContainer: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: isHeightSmall ? 70 : 75,
+    // borderWidth: 1,
+    // marginTop: 20,
+  },
+  Inputs: {
     flexDirection: "row",
-    justifyContent: "space-evenly",
+    justifyContent: "space-between",
+    width: isWidthSmall ? 275 : 286,
   },
   Input: {
-    width: 40,
-    height: 50,
+    width: isWidthSmall ? 38 : 40,
+    height: isWidthSmall ? 48 : 50,
     borderRadius: 7,
+    borderWidth: 1,
+  },
+  Buttons: {
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    // borderWidth: 1,
+    height: isHeightSmall ? 75 : 85,
+  },
+  Header: {
+    height: 45,
+    // borderWidth: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  PhoneNumber: {
+    flexDirection: "row",
+    justifyContent: "center",
   },
 });
